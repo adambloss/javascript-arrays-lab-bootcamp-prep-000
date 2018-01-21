c7d02cda9664c8c8799fba5f978baae6778c50f2
Adam Bloss <adam.bloss17@gmail.com>
2018-01-21 08:50:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  return kittens.slice(0,kitten.length-2)
+  return kittens - kittens.slice(-1)
 }
 function removeFirstKitten() {
   return kittens.slice(1)
